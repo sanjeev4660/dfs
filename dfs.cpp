0a36171dd9257b308ade5ecea98d5fa529c92290
sanjeev4660 <60230449+sanjeev4660@users.noreply.github.com>
2021-01-16 14:15:43 +0530

    Add files via upload

---
+++ dfs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+vector<int>adj[10000];
+vector<int>vis[10000];
+
+
+void dfs(int node){
+    vis[node]=1;
+    cout<<node<" ";
+
+    for(auto it: adj[node]){
+        if(vis[it]==0){
+            dfs(it);
+        }
+    }
+}
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);   
+        adj[v].push_back(u);     
+    }
+    for(int i=1;i<=n;i++){
+        if(vis[i]==0){
+            dfs(i);
+        }
+    }
+}
